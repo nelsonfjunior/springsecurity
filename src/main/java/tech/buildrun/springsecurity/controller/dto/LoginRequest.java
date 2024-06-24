7d6884f331841486f6f720e4b05bfc86a28150f4
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-06-24 10:03:39 -0300

    create login's dtos

---
+++ src/main/java/tech/buildrun/springsecurity/controller/dto/LoginRequest.java
@@ -0,0 +1,5 @@
+package tech.buildrun.springsecurity.controller.dto;
+
+public record LoginRequest(String username, String password) {
+    
+}
---
+++ src/main/java/tech/buildrun/springsecurity/controller/dto/LoginResponse.java
@@ -0,0 +1,5 @@
+package tech.buildrun.springsecurity.controller.dto;
+
+public record LoginResponse(String accessToken, Long expiresIn) {
+    
+}
